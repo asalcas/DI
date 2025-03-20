0197b63ae3b7c7fb897fc270cf2a2c26a9492db5
BaiHuu <alvaro.salvador@iesnervion.es>
2025-03-20 14:14:16 +0100

    Primer poyecto YellowStone

---
+++ YellowStone/ENT/ClsCaballo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class ClsCaballo
+    {
+        #region Atributos
+
+        private int idCaballo;
+
+        #endregion
+
+        #region Propiedades
+        
+         public int IdCaballo
+        {
+            get
+            {
+                return idCaballo;
+            }
+
+        }
+        // EL Equivalente para java seria: 
+
+        //public int getIdCaballo()
+        //{
+        //    return idCaballo;
+        //}
+
+        
+
+        // PROPIEDAD AUTOIMPLEMENTADA
+
+        public string Nombre { get; set; } // El unico inconveniente de esto, es que no podemos meter codigo dentro
+        public int IdRaza { get; set; }
+        #endregion
+
+        #region Constructores
+
+        public ClsCaballo()
+        {
+
+        }
+        public ClsCaballo(int idCaballo, string Nombre, int IdRaza)
+        {
+            this.idCaballo = idCaballo;
+            this.Nombre = Nombre;
+            this.IdRaza = IdRaza;
+        }
+
+
+    }
+}
---
+++ YellowStone/ENT/ClsRaza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class ClsRaza
+    {
+        #region propiedades
+        public int IdRaza { get; }
+        public String Nombre { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ClsRaza()
+        {
+
+        }
+        public ClsRaza(int idRaza, String nombre)
+        {
+            this.IdRaza = idRaza;
+            this.Nombre = nombre;
+        }
+        #endregion
+
+    }
+}
