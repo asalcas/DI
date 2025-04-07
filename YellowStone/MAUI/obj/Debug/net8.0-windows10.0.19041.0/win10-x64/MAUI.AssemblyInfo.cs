9ab4acbcdfa82c262eda763a08e67f8a929a17dc
BaiHuu <alvaro.salvador@iesnervion.es>
2025-04-07 02:12:14 +0200

    .

--- YellowStone/MAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MAUI.AssemblyInfo.cs
+++ YellowStone/MAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MAUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8b68fc3fd9c53112ffc175afe0465eb7750483bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7644874cfae188bbf72e3af207afeb091a446c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
